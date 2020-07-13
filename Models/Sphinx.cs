4f9f310843bf395b87366efa13508c6c943c3b41
Jeff Kim <kim2jy@hotmail.com>
2020-07-13 15:33:42 -0700

    Initial commit, add public properties to class
    co-authored-by: Teresa Rosinski <trosinski89@gmail.com>
    Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>

---
+++ Models/Sphinx.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collection.Generic;
+
+namespace Sphinx 
+{
+  //   public string MakeModel { get; set; }
+  //   public int Price { get; set; }
+  //   public int Miles { get; set; }
+
+  //   // Constructor
+
+  //   public Car(string makeModel, int price, int miles)
+  //   {
+  //       MakeModel = makeModel;
+  //       Price = price;
+  //       Miles = miles;
+  //   }
+    public class Riddle 
+  {
+    public string Question { get; set; }
+    public string Answer { get; set; }
+  
+  }  
+}
